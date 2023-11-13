3dbe926312043f76875747109ceb0b5b6efc736c
IvanLow <IVANLOW.22@ichat.sp.edu.sg>
2023-11-13 14:05:38 +0800

    BMI Calulator

---
+++ Exercise1.py
@@ -0,0 +1,19 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = float(weight) / pow(float(height), 2)
+    print("BMI = " + str(bmi))
+    print("Weight Classification:")
+    if bmi < 18.5:
+        print("Under Weight")
+        return -1
+    elif bmi >= 18.5 and bmi <= 25.0:
+        print("Normal Weight")
+        return 0
+    else:
+        print("Over Weight")
+        return 1
+
+
+calculate_bmi(weight=47, height=1.54)
